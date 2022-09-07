fd5e1d1af278e22e379460ac2225a4102b499dc4
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-09-07 16:27:25 +0300

    add splash screen and primary color to app

---
+++ IssueTracker/IssueTracker/Helpers/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  IssueTracker
+//
+//  Created by Stavros Tsikinas on 7/9/22.
+//
+// In this file all extensions of the project are placed.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - UIKit Extensions
+
+// UIColor
+extension UIColor {
+    
+    static var primaryColor: UIColor {
+        UIColor(red: 0.054, green: 0.042, blue: 0.51, alpha: 1.0)
+    }
+    
+}
